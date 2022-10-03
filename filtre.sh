4b94ac65f9a2ae02dc4b0e496d235692831f2fde
Joancc04 <joancolillas2004@gmail.com>
2022-10-03 17:26:09 +0200

    fi primer dia

--- filtre.sh
+++ filtre.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+arx= tail -n +2 "netflix_titles.csv"
 while IFS ="," read -r id  type title director 
 do
 	echo "Tipus: $type"
 	echo "Títol: $title"
 	echo "Director: $director"
 
-done < (tail -n +2 netflix_titles.csv)
+#done<(tail -n +2 netflix_titles.csv)
+done<arx
+
